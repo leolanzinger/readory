c31561253a0ee51eb81a14d4f06add4fa37de4cc
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2016-01-30 18:08:11 +0100

    select players menu

--- Readory_test/ViewController.swift
+++ Readory_test/ViewController.swift
@@ -9,36 +9,48 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var countLabel: CountLabel!
     
+    var selectedPlayers:Int!
     var players:Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func onePlayerSelected(sender: AnyObject) {
-        self.players = 1
-        performSegueWithIdentifier("startGame", sender: self)
-    }
-    @IBAction func twoPlayersSelected(sender: AnyObject) {
-        self.players = 2
-        performSegueWithIdentifier("startGame", sender: self)
+    @IBAction func onePlayer(sender: AnyObject) {
+        self.selectedPlayers = 1
+        performSegueWithIdentifier("confirmGame", sender: self)
     }
     
-    @IBAction func threePlayersSelected(sender: AnyObject) {
-        self.players = 3
-        performSegueWithIdentifier("startGame", sender: self)
+    
+    @IBAction func twoPlayers(sender: AnyObject) {
+        self.selectedPlayers = 2
+        performSegueWithIdentifier("confirmGame", sender: self)
     }
     
-    @IBAction func fourPlayersSelected(sender: AnyObject) {
-        self.players = 4
-        performSegueWithIdentifier("startGame", sender: self)
+    @IBAction func threePlayers(sender: AnyObject) {
+        self.selectedPlayers = 3
+        performSegueWithIdentifier("confirmGame", sender: self)
     }
-
+    
+    @IBAction func fourPlayers(sender: AnyObject) {
+        self.selectedPlayers = 4
+        performSegueWithIdentifier("confirmGame", sender: self)
+    }
+    
+    @IBAction func fivePlayers(sender: AnyObject) {
+        self.selectedPlayers = 5
+        performSegueWithIdentifier("confirmGame", sender: self)
+    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if (segue.identifier == "startGame") {
             Game.init()
             Game.sharedInstance.setPlayers(self.players)
+        }
+        else if (segue.identifier == "confirmGame") {
+            let secondViewController = segue.destinationViewController as! ViewController
+            let plyrs = self.selectedPlayers as Int
+            secondViewController.players = plyrs
         }
     }
     
